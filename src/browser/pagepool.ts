9a8b08fd6005ccfc1932c3c9487662dd119dbc73
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2025-01-19 00:50:23 +0530

    make some deployment changes

--- src/browser/pagepool.ts
+++ src/browser/pagepool.ts
@@ -46,7 +46,7 @@ export default class PagePool {
     const launchOptions = {
       acceptInsecureCerts: true,
       headless: process.env.DEBUG !== "true",
-      executablePath: 'executablePath()',
+      executablePath: executablePath(),
       userDataDir: "/tmp/translateer-data",
       args: [
         "--no-sandbox",
